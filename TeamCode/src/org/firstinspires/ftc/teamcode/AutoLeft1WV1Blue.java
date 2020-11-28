71166185eaaa950948a1095bc5afae60c2de3066
Thanish <js2t@outlook.com>
2020-11-27 20:41:48 -0500

    Fixed autonomous programs

--- TeamCode/src/org/firstinspires/ftc/teamcode/AutoLeft1WV1Blue.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/AutoLeft1WV1Blue.java
@@ -5,12 +5,12 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name="Left1WobbleTarget", group="Motion")
-public class AutoLeft1WV1 extends LinearOpMode {
+@Autonomous(name="Left1WobbleTargetBlue", group="Motion")
+public class AutoLeft1WV1Blue extends LinearOpMode {
     // This program starts on the right line, drops off a wobble goal in it's target,
     // Goes to shoot at the center shooting spot, grabs second wobble goal and drops in it's target,
     // than drives to center shooting spot to park at the end.
-    char ringAt = 'A';
+    char ringAt;
     ProgrammingFrame robot   = new ProgrammingFrame();
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -22,58 +22,44 @@ public class AutoLeft1WV1 extends LinearOpMode {
         // the Tower Goal, Power Shot Targets, or some mixture of both
         // Driving to the starter stack
         robot.GoDistanceCM(59, .8, this);
-        System.out.println("forward 59");
-        robot.StrafeCM(35, .8, this);
-        System.out.println("strafe 35");
+        robot.StrafeCM(-35, .8, this);
         // Detect the rings here and return A, B, C, or E for Error
-        //ringAt = robot.ringFinder();
+        ringAt = 'A';
         if (ringAt == 'E') {  // Top saw a ring but bottom didn't somehow, taking C path is safest
             ringAt = 'C';
         }
-
         // Gets us to the target zone
-        robot.StrafeCM(59, 0.8, this);
-        System.out.println("strafe 59");
-        System.exit(0);
+        robot.StrafeCM(59, .8, this);
         if (ringAt == 'A') {
             robot.GoDistanceCM(59, .8, this);
-            System.out.println("forward 59");
         }
         else if (ringAt == 'B') {
             robot.GoDistanceCM(118, .8, this);
-            System.out.println("forward 118");
-            robot.StrafeCM(-59, .8, this);
-            System.out.println("B");
+            robot.StrafeCM(59, .8, this);
         }
         else {
             robot.GoDistanceCM(177, .8, this);
         }
         // Add function that drops a wobble goal
         // Move to the launch line
-
         if (ringAt == 'A') {
-            robot.StrafeCM(-59, .8, this);
-            System.out.println("back 59");
+            robot.StrafeCM(59, .8, this);
             robot.GoDistanceCM(27, .8, this);
-            System.out.println("forward 27");
         }
         else if (ringAt == 'B') {
             robot.GoDistanceCM(-27, .8, this);
         }
         else {
             robot.GoDistanceCM(-86, .8, this);
-            robot.StrafeCM(-59, .8, this);
+            robot.StrafeCM(59, .8, this);
         }
-
         // Comment out the below if we don't have time!!!
         // Use our sensor to make sure we are on the line
         // Backup to make sure we are behind the line
-        robot.GoDistanceCM(-15, .8, this);
-        System.out.println("backward 15");
+        //robot.GoDistanceCM(-15, .8, this);
         // Line up to the line
-       // robot.findLine(.5);
+        //robot.findLine(.5);
         // Go forward a tiny bit that way we are more centered on the line
-        robot.GoDistanceCM(5, .8, this);
-        System.out.println("forward 5");
+        //robot.GoDistanceCM(5, .8, this);
     }
 }
--- TeamCode/src/org/firstinspires/ftc/teamcode/AutoLeft2WV1Blue.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/AutoLeft2WV1Blue.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name="Left1WobbleTarget", group="Motion")
-public class AutoLeft1WV1 extends LinearOpMode {
+@Autonomous(name="Left2WobbleTargetsBlue", group="Motion")
+public class AutoLeft2WV1Blue extends LinearOpMode {
     // This program starts on the right line, drops off a wobble goal in it's target,
     // Goes to shoot at the center shooting spot, grabs second wobble goal and drops in it's target,
     // than drives to center shooting spot to park at the end.
@@ -22,58 +22,76 @@ public class AutoLeft1WV1 extends LinearOpMode {
         // the Tower Goal, Power Shot Targets, or some mixture of both
         // Driving to the starter stack
         robot.GoDistanceCM(59, .8, this);
-        System.out.println("forward 59");
-        robot.StrafeCM(35, .8, this);
-        System.out.println("strafe 35");
+        robot.StrafeCM(-35, .8, this);
         // Detect the rings here and return A, B, C, or E for Error
-        //ringAt = robot.ringFinder();
+
         if (ringAt == 'E') {  // Top saw a ring but bottom didn't somehow, taking C path is safest
             ringAt = 'C';
         }
-
         // Gets us to the target zone
-        robot.StrafeCM(59, 0.8, this);
-        System.out.println("strafe 59");
-        System.exit(0);
+        robot.StrafeCM(-59, .8 , this);
         if (ringAt == 'A') {
             robot.GoDistanceCM(59, .8, this);
-            System.out.println("forward 59");
         }
         else if (ringAt == 'B') {
             robot.GoDistanceCM(118, .8, this);
-            System.out.println("forward 118");
-            robot.StrafeCM(-59, .8, this);
-            System.out.println("B");
+            robot.StrafeCM(59, .8, this);
         }
         else {
             robot.GoDistanceCM(177, .8, this);
         }
         // Add function that drops a wobble goal
         // Move to the launch line
-
         if (ringAt == 'A') {
-            robot.StrafeCM(-59, .8, this);
-            System.out.println("back 59");
+            robot.StrafeCM(59, .8, this);
             robot.GoDistanceCM(27, .8, this);
-            System.out.println("forward 27");
         }
         else if (ringAt == 'B') {
             robot.GoDistanceCM(-27, .8, this);
         }
         else {
             robot.GoDistanceCM(-86, .8, this);
+            robot.StrafeCM(59, .8, this);
+        }
+        // Go to pick up other wobble goal
+        robot.StrafeCM(59, .8, this);
+        robot.GoDistanceCM(-144, .8, this);
+        robot.RotateDEG(-90, .8, this);
+        // Add function that picks up that wobble goal
+        // Drive back to the target zones to drop off the second wobble goal
+        robot.RotateDEG(90, .8, this);
+        robot.GoDistanceCM(144, .8, this);
+        if (ringAt == 'A') {
+            robot.StrafeCM(-118, .8, this);
+        }
+        else if (ringAt == 'B') {
+            robot.GoDistanceCM(59, .8, this);
             robot.StrafeCM(-59, .8, this);
         }
-
+        else {
+            robot.GoDistanceCM(118, .8, this);
+            robot.StrafeCM(-118, .8, this);
+        }
+        // use the method(s) to drop the wobble goal
+        // Drive to the launch line (in particular the place we shot from)
+        if (ringAt == 'A') {
+            robot.StrafeCM(59,0.8, this);  // Could be less if time is real close
+            robot.GoDistanceCM(27, .8, this);
+        }
+        else if (ringAt == 'B') {
+            robot.GoDistanceCM(-27, .8, this);
+        }
+        else {
+            robot.GoDistanceCM(-86, .8, this);
+            robot.StrafeCM(59, .8, this);  // Can be removed if not enough time
+        }
         // Comment out the below if we don't have time!!!
         // Use our sensor to make sure we are on the line
         // Backup to make sure we are behind the line
-        robot.GoDistanceCM(-15, .8, this);
-        System.out.println("backward 15");
+        //robot.GoDistanceCM(-15, .8, this);
         // Line up to the line
-       // robot.findLine(.5);
+        //robot.findLine(.5);
         // Go forward a tiny bit that way we are more centered on the line
-        robot.GoDistanceCM(5, .8, this);
-        System.out.println("forward 5");
+        //robot.GoDistanceCM(5, .8, this);
     }
 }
